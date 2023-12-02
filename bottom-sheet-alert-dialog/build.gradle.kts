fa1f1bb747d2b00de72ba8e5e82dd411236bfc48
bruh <quangtrung02hn16@gmail.com>
2023-12-02 22:13:55 +0700

    Bump deps

--- bottom-sheet-alert-dialog/build.gradle.kts
+++ bottom-sheet-alert-dialog/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs_minimal:2.0.3")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs_minimal:2.0.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
