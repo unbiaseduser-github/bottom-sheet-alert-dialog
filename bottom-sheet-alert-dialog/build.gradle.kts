7ac6bd4654bb1638c9c1f306d8f357d0ea5ac7b3
bruh <quangtrung02hn16@gmail.com>
2024-06-06 11:08:09 +0700

    Bump stuff

--- bottom-sheet-alert-dialog/build.gradle.kts
+++ bottom-sheet-alert-dialog/build.gradle.kts
@@ -80,9 +80,9 @@ afterEvaluate {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs_minimal:2.0.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
