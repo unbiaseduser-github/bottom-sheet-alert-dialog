be397d192b9558b131695f9ee3f660bfeb94d3b1
bruh <quangtrung02hn16@gmail.com>
2024-09-08 21:54:15 +0700

    Remove unused proguard files

--- bottom-sheet-alert-dialog/build.gradle.kts
+++ bottom-sheet-alert-dialog/build.gradle.kts
@@ -19,18 +19,8 @@ android {
         minSdk = 21
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     buildFeatures {
         viewBinding = true
     }
