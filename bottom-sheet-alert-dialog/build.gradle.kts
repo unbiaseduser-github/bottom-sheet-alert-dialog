039aec748ee4a1e9c346fa147de04b80d729dbf2
bruh <quangtrung02hn16@gmail.com>
2024-09-15 16:35:35 +0700

    Bump version

--- bottom-sheet-alert-dialog/build.gradle.kts
+++ bottom-sheet-alert-dialog/build.gradle.kts
@@ -39,7 +39,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.sixtyninefourtwenty"
                 artifactId = "bottom-sheet-alert-dialog"
-                version = "1.3.0"
+                version = "2.0.0"
 
                 from(components["release"])
 
